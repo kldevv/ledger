9a8be14063597c5e3bc34bff9ab61b115cfb0c47
Kuan-You Lin <linstudyhard1997@gmail.com>
2023-11-20 22:52:28 -0500

    Add route

---
+++ src/utils/route.ts
@@ -0,0 +1,17 @@
+export const route = {
+  "transaction": {
+    "index": '/transaction'
+  },
+  "statement": {
+    "index": '/statement'
+  },
+  "people": {
+    "index": "/people",
+  },
+  "group": {
+    "index": "/group"
+  },
+  "account": {
+    "index": "/account"
+  }
+} as const
