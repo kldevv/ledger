68732ac5d87190292d4a295e8bc4e0e2a5e06de8
Kuan-You Lin <linstudyhard1997@gmail.com>
2023-11-26 14:08:36 -0500

    Add misc progress

--- src/components/transaction/TransactionTable/TransactionTable.tsx
+++ src/components/transaction/TransactionTable/TransactionTable.tsx
@@ -4,33 +4,86 @@ import {
   getCoreRowModel,
   useReactTable,
 } from '@tanstack/react-table';
-import { Transaction } from '@prisma/client';
+import { Currency, Transaction } from '@prisma/client';
 import { useTranslation } from 'react-i18next';
-import { useMemo } from 'react';
-import { TransactionTableRow } from './Row';
+import Link from 'next/link';
 
-export type TransactionTableProps = {
-  /**
-   * Data feed for the transactiontable
-   */
-  transactions: Transaction[]
-}
+const data: Transaction[] = [
+  {
+    id: 1,
+    description: 'hello',
+    accruedDate: new Date(0),
+    currency: Currency.USD,
+    createdDate: new Date(0),
+    updatedDate: new Date(0),
+  },
+  {
+    id: 2,
+    description: 'world',
+    accruedDate: new Date(0),
+    currency: Currency.USD,
+    createdDate: new Date(0),
+    updatedDate: new Date(0),
+  },
+];
 
-export const TransactionTable: React.FC<TransactionTableProps> = ({ transactions }) => {
+
+export const TransactionTable: React.FC = () => {
   const { t } = useTranslation('transaction')
+
+  const columnHelper = createColumnHelper<Transaction>();
+  const columns = [
+    columnHelper.accessor('id', {
+      header: t('table.header.id'),
+      cell: (info) => <span>{info.getValue()}</span>,
+    }),
+    columnHelper.accessor('description', {
+      header: t('table.header.description'),
+      cell: (info) => <span>{info.getValue()}</span>,
+    }),
+    columnHelper.accessor('accruedDate', {
+      header: 'Date',
+      cell: (info) => <span>{info.getValue().toDateString()}</span>,
+    }),
+  ];
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
 
   return (
     <table>
       <thead>
-        <tr>
-          <th></th>
-          <th>{t('table.header.id')}</th>
-          <th>{t('table.header.title')}</th>
-          <th>{t('table.header.date')}</th>
-          <th></th>
-        </tr>
+        {table.getHeaderGroups().map((headerGroup) => (
+          <tr key={headerGroup.id}>
+            {headerGroup.headers.map((header) => (
+              <th key={header.id}>
+                {header.isPlaceholder
+                  ? null
+                  : flexRender(
+                      header.column.columnDef.header,
+                      header.getContext()
+                    )}
+              </th>
+            ))}
+          </tr>
+        ))}
       </thead>
-      <tbody>{transactions.map((transaction) => <TransactionTableRow key={transaction.id} {...transaction} />)}</tbody>
+      <tbody>
+        {table.getRowModel().rows.map((row) => (
+          <Link href={`/transaction/${123}`}>
+            <tr key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <td key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          </Link>
+        ))}
+      </tbody>
     </table>
   );
 };
