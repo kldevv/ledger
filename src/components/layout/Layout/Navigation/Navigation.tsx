5b4b69bdb248e0a25fd053169b1ffe4c187ae982
Kuan-You Lin <linstudyhard1997@gmail.com>
2023-11-30 18:44:30 -0500

    Update style

--- src/components/layout/Layout/Navigation/Navigation.tsx
+++ src/components/layout/Layout/Navigation/Navigation.tsx
@@ -7,7 +7,7 @@ import Link from 'next/link';
  */
 export const Navigation: React.FC = () => {
   return (
-    <div className="flex flex-col w-72 bg-white">
+    <div className="flex flex-col w-64 bg-white">
       <div className="px-6 gap-y-5 border-r overflow-y-auto flex flex-col grow">
         <div className="flex shrink-0 h-16 items-center">
           <Link href="/">Brand</Link>
