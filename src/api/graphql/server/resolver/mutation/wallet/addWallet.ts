5296b010fb8f2ed2b8b49860c756c24f159128f3
Kuan-You Lin <linstudyhard1997@gmail.com>
2023-12-07 12:45:15 -0500

    Fix resolver

--- src/api/graphql/server/resolver/mutation/wallet/addWallet.ts
+++ src/api/graphql/server/resolver/mutation/wallet/addWallet.ts
@@ -3,7 +3,7 @@ import { MutationResolvers } from "@/api/graphql";
 export const addWallet: MutationResolvers['addWallet'] = async (
   _, { input }, { dataSources: { prisma }}
 ) => {
-  const wallet = (await prisma.walletDS.createOne(input))
+  const wallet = (await prisma.wallet.createOne(input))
 
   return wallet
 }
