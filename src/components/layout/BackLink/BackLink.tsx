32ea655af809b35487b2b5eded3a12c3dd07ff95
Kuan-You Lin <linstudyhard1997@gmail.com>
2023-12-09 12:20:50 -0500

    Add back link translate

--- src/components/layout/BackLink/BackLink.tsx
+++ src/components/layout/BackLink/BackLink.tsx
@@ -1,5 +1,6 @@
 import { ChevronLeftIcon } from "@heroicons/react/20/solid"
 import Link from "next/link"
+import { useTranslation } from "react-i18next";
 import { UrlObject } from "url";
 
 export type BackButtonProps = {
@@ -10,11 +11,13 @@ export type BackButtonProps = {
 };
 
 export const BackLink: React.FC<BackButtonProps> = ({ href }) => {
+  const { t } = useTranslation('layout')
+
   return (
     <Link href={href} className="-ml-5">
       <div className="text-darkMidGray flex items-center gap-x-1 font-normal text-sm h-4">
         <ChevronLeftIcon className="w-4 h-4" />
-        {href === '/' ? 'Home' : 'Back'}
+        {href === '/' ? t('back-link.label.home') : t('back-link.label.back')}
       </div>
     </Link>
   );
