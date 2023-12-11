d584361c1cec1b2053c20fd8360f7e15cd76c935
Kuan-You Lin <linstudyhard1997@gmail.com>
2023-12-11 15:46:55 -0500

    Rename light-accent

--- src/components/vault/VaultRadioCards/VaultRadioCard/VaultRadioCard.tsx
+++ src/components/vault/VaultRadioCards/VaultRadioCard/VaultRadioCard.tsx
@@ -24,7 +24,7 @@ export const VaultRadioCard: React.FC<VaultRadioCardProps> = ({
   return (
     <Item
       value={id}
-      className="ring-1 data-[state=checked]:ring-2 hover:ring-2 data-[state=checked]:ring-lightAccent hover:ring-lightAccent ring-darkMidGray rounded-md h-16"
+      className="ring-1 data-[state=checked]:ring-2 hover:ring-2 data-[state=checked]:ring-light-accent hover:ring-light-accent ring-darkMidGray rounded-md h-16"
     >
       <div className="mx-3 flex items-center">
         <div className="flex flex-col text-darkShades items-start">
@@ -36,7 +36,7 @@ export const VaultRadioCard: React.FC<VaultRadioCardProps> = ({
           </p>
           <p className="text-darkShades text-xs font-normal">{currency}</p>
         </div>
-        <Indicator className="text-lightAccent ml-auto">
+        <Indicator className="text-light-accent ml-auto">
           <CheckCircleIcon className="w-6 h-6" />
         </Indicator>
       </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,7 +19,7 @@ const config: Config = {
 
       // main color palette
       main: 'rgb(172,88,80)',  // #AC5850
-      lightAccent: 'rgb(226,126,97)',  // #E27E61
+      "light-accent": 'rgb(226,126,97)',  // #E27E61
       lightShades: 'rgb(250,249,248)',  // #FAF9F8
       darkAccent: 'rgb(176,135,158)',  // #B0879E
       darkShades: 'rgb(51,48,71)',  // #333047
