e97642a2a4babc746f2db42c613ad7f8b5473ce7
Kuan-You Lin <linstudyhard1997@gmail.com>
2023-12-11 15:45:39 -0500

    Change hex color to rgb color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,22 +12,22 @@ const config: Config = {
       current: 'currentColor',
       
       // base color palette
-      white: '#FFFFFF',
-      lightGray: '#f9fafb',
-      midGray: '#e5e7eb',
-      darkMidGray: '#9ca3af',
+      white: 'rgb(255,255,255)',  // #FFFFFF
+      lightGray: 'rgb(249,250,251)',  // #F9FAFB
+      midGray: 'rgb(229,231,235)',  // #E5E7EB
+      darkMidGray: 'rgb(156,163,175)',  // #9CA3AF
 
       // main color palette
-      main: '#AC5850',
-      lightAccent: '#E27E61',
-      lightShades: '#FAF9F8',
-      darkAccent: '#B0879E',
-      darkShades: '#333047',
+      main: 'rgb(172,88,80)',  // #AC5850
+      lightAccent: 'rgb(226,126,97)',  // #E27E61
+      lightShades: 'rgb(250,249,248)',  // #FAF9F8
+      darkAccent: 'rgb(176,135,158)',  // #B0879E
+      darkShades: 'rgb(51,48,71)',  // #333047
       
       // rainbow color palette
-      'red': 'rgb(255, 96, 92)',  //#FF605C
-      'yellow': 'rgb(255, 189, 68)', //#FFBD44
-      'green': 'rgb(0, 202, 78)', //#FFBD44
+      'red': 'rgb(255,96,92)',  // #FF605C
+      'yellow': 'rgb(255,189,68)', // #FFBD44
+      'green': 'rgb(0,202,78)', // #FFBD44
 
       // rainbow halo color palette 
       'red-halo': 'rgb(255, 96, 92, 0.1)',  //#FF605C
