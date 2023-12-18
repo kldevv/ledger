73a0113d3d1070464649dbee37bd6948cfe9479b
Kuan-You Lin <linstudyhard1997@gmail.com>
2023-12-18 12:06:17 -0500

    Add add tag resolver

---
+++ src/api/graphql/server/resolver/mutation/tag/addTag.ts
@@ -0,0 +1,7 @@
+import { MutationResolvers } from "@/api/graphql";
+
+export const addTag: MutationResolvers['addTag'] = async (
+  _, { input }, { dataSources: { prisma } }
+) => {
+  return await prisma.tag.createOne(input)
+}
