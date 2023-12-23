0d229a350d025c57c3cf289f6c2f1fc8186fba82
Kuan-You Lin <linstudyhard1997@gmail.com>
2023-12-23 15:38:35 -0500

    Update entry page translate

--- src/pages/entry/index.tsx
+++ src/pages/entry/index.tsx
@@ -1,12 +1,15 @@
 import { EntryDashboard } from '@/components/entry';
 import { PageHeader, Layout } from '@/components/layout';
+import { useTranslation } from 'next-i18next';
 
 const Page: React.FC = () => {
+  const { t } = useTranslation('entry')
+
   return (
     <Layout prev="/">
       <PageHeader
-        title="Entries"
-        subtitle="All entries for the currently selected vault."
+        title={t`page.index.title`}
+        subtitle={t`page.index.subtitle`}
       />
       <EntryDashboard />
     </Layout>
