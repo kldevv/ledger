4a5e22b1699840102a5ff2fe4301fcffa0b07486
Kuan-You Lin <linstudyhard1997@gmail.com>
2023-12-30 18:45:32 -0500

    Add radio group

---
+++ src/components/common/RadioGroup/RadioGroup.tsx
@@ -0,0 +1,74 @@
+import { useCallback, useState } from 'react';
+import { RadioGroup as HeadlessRadioGroup } from '@headlessui/react';
+
+export interface RadioGroupProps {
+  /**
+   * Radio group label
+   */
+  label: string;
+  /**
+   * Options
+   */
+  options: {
+    /**
+     * Option key
+     */
+    value: string;
+    /**
+     * Option label
+     */
+    label: string;
+  }[];
+  /**
+   * On change
+   */
+  onChange?: () => void
+}
+
+export const RadioGroup: React.FC<RadioGroupProps> = ({
+  options,
+  label,
+  onChange,
+}) => {
+  const [selected, setSelected] = useState(options[0].value);
+
+  const handleOnChange = useCallback(
+    (value: RadioGroupProps['options'][number]['value']) => {
+      onChange?.();
+      setSelected(value);
+    },
+    [onChange, setSelected]
+  );
+
+  return (
+    <HeadlessRadioGroup value={selected} onChange={handleOnChange}>
+      <HeadlessRadioGroup.Label className="sr-only">
+        {label}
+      </HeadlessRadioGroup.Label>
+      <div className="flex">
+        {options.map(({ label, value }) => (
+          <HeadlessRadioGroup.Option
+            key={value}
+            value={value}
+            className={({ checked }) =>
+              `flex focus:outline-none cursor-pointer first:rounded-l-lg last:rounded-r-lg border px-12 bg-white ${
+                checked ? 'border-light-accent' : 'border-mid-gray'
+              }`
+            }
+          >
+            {({ checked }) => (
+              <div className="flex items-center text-sm font-medium leading-6">
+                <HeadlessRadioGroup.Label
+                  as="button"
+                  className={`${checked ? 'text-light-accent' : 'text-gray'}`}
+                >
+                  {label}
+                </HeadlessRadioGroup.Label>
+              </div>
+            )}
+          </HeadlessRadioGroup.Option>
+        ))}
+      </div>
+    </HeadlessRadioGroup>
+  );
+};
--- src/components/report/ChangesReport/ChangesReport.tsx
+++ src/components/report/ChangesReport/ChangesReport.tsx
@@ -1,19 +1,39 @@
 import { Basis, useGetMonthlyReportsQuery } from '@/api/graphql';
 import { useVaultContext } from '@/hooks';
 import { AccountTopologyTable } from '..';
+import { useCallback, useState } from 'react';
+import { RadioGroup } from '@/components/common';
 
 export const ChangesReport: React.FC = () => {
   const [{ curVaultId }] = useVaultContext();
+  const [ isAccrualBasis, setIsAccrualBasis ] = useState(true)
 
   const { data, error } = useGetMonthlyReportsQuery({
     variables: {
       input: {
         vaultId: curVaultId ?? '',
         year: 2023,
+        basis: isAccrualBasis ? Basis.ACCRUAL : Basis.CASH
       },
     },
     skip: curVaultId == null,
   });
 
-  return <AccountTopologyTable reportData={data?.getMonthlyReports ?? []} />;
+  const handleOnChange = useCallback(() => {
+    setIsAccrualBasis((prev) => !prev);
+  }, [setIsAccrualBasis]);
+
+  return (
+    <div>
+      <RadioGroup
+        options={[
+          { label: 'Accrual', value: '1' },
+          { label: 'Cash', value: '0' },
+        ]}
+        label="ACCOUNTING BASIS"
+        onChange={handleOnChange}
+      />
+      <AccountTopologyTable reportData={data?.getMonthlyReports ?? []} />
+    </div>
+  );
 };
