8c526eb436f50e6137d4a840b3688ab2c2318a34
Kuan-You Lin <linstudyhard1997@gmail.com>
2024-01-03 00:52:58 -0500

    Add read min max accrual date

--- src/db/prisma/dao/entry/stats/stats.ts
+++ src/db/prisma/dao/entry/stats/stats.ts
@@ -1,11 +1,11 @@
 import prisma from "@/db/prisma/client"
 import { Entry } from "@prisma/client"
 
-export namespace ReadMinMaxTransactionYear {
+export namespace ReadMinMaxTransactionDate {
   export type Args = Pick<Entry, 'vaultId'>
 }
 
-export const readMinMaxTransactionYear = async ({ vaultId }: ReadMinMaxTransactionYear.Args) => {
+export const readMinMaxTransactionDate = async ({ vaultId }: ReadMinMaxTransactionDate.Args) => {
   try {
     return await prisma.entry.aggregate({
       where: {
---
+++ src/db/prisma/dao/transaction/stats/stats.ts
@@ -0,0 +1,24 @@
+import prisma from "@/db/prisma/client"
+import { Transaction } from "@prisma/client"
+
+export namespace ReadMinMaxAccrualDate {
+  export type Args = Pick<Transaction, 'vaultId'>
+}
+
+export const readMinMaxAccrualDate = async ({ vaultId }: ReadMinMaxAccrualDate.Args) => {
+  try {
+    return await prisma.transaction.aggregate({
+      where: {
+        vaultId
+      },
+      _min: {
+        accrualDate: true
+      },
+      _max: {
+        accrualDate: true
+      }
+    })
+  } catch (e) {
+
+  }
+}
