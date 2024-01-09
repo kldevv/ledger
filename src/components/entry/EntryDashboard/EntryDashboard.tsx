da14eb306986abd0ac8005907c33d0a6fdb73701
Kuan-You Lin <linstudyhard1997@gmail.com>
2024-01-08 22:49:17 -0500

    Update entry table

--- src/components/entry/EntryDashboard/EntryDashboard.tsx
+++ src/components/entry/EntryDashboard/EntryDashboard.tsx
@@ -1,21 +1,17 @@
-import { useGetEntriesQuery } from "@/api/graphql";
-import { useVaultContext } from "@/hooks";
-import { EntryTable } from "..";
+import { useGetEntriesQuery } from '@/api/graphql'
+import { EntryTable } from '@/components/entry'
+import { useVaultContext } from '@/hooks'
 
 export const EntryDashboard: React.FC = () => {
-  const [{ curVaultId }] = useVaultContext();
-  const {
-    data,
-    loading,
-    error,
-  } = useGetEntriesQuery({
+  const [{ curVaultId }] = useVaultContext()
+  const { data } = useGetEntriesQuery({
     variables: {
       input: {
         vaultId: curVaultId ?? '',
       },
     },
-    skip: curVaultId == null
-  });
+    skip: curVaultId == null,
+  })
 
   return <EntryTable data={data?.getEntries ?? []} />
 }
--- src/components/entry/EntryTable/EntryTable.tsx
+++ src/components/entry/EntryTable/EntryTable.tsx
@@ -1,52 +1,61 @@
-import { createColumnHelper } from '@tanstack/react-table';
-import { useTranslation } from 'next-i18next';
-import { GetEntriesQuery } from '@/api/graphql';
-import { FormattedDate, StatusChip, Table } from '@/components/common';
+import { createColumnHelper } from '@tanstack/react-table'
+import { useTranslation } from 'next-i18next'
+import { useMemo } from 'react'
+
+import { FormattedDate, StatusChip, Table } from '@/components/common'
+
+import type { GetEntriesQuery } from '@/api/graphql'
 
 export type EntryTableData = GetEntriesQuery['getEntries'][number]
 
-const columnHelper = createColumnHelper<EntryTableData>();
+const columnHelper = createColumnHelper<EntryTableData>()
 
 export type EntryTableProps = {
   /**
    * Table data
    */
-  data: EntryTableData[];
-};
+  data: EntryTableData[]
+}
 
 export const EntryTable: React.FC<EntryTableProps> = ({ data }) => {
-  const { t } = useTranslation('entry');
+  const { t } = useTranslation('entry')
 
-  const colDefs = [
-    columnHelper.accessor('transactionDate', {
-      header: t('EntryTable.header.date'),
-      cell: (props) => (
-          <FormattedDate dateTime={props.getValue()} className='text-dark-shades'/>
-      ),
-    }),
-    columnHelper.accessor('debit', {
-      header: t('EntryTable.header.debit'),
-    }),
-    columnHelper.accessor('credit', {
-      header: t('EntryTable.header.credit'),
-    }),
-    columnHelper.accessor('account.name', {
-      header: t('EntryTable.header.account'),
-    }),
-    columnHelper.accessor('memo', {
-      header: t('EntryTable.header.memo'),
-    }),
-    columnHelper.accessor('status', {
-      header: t('EntryTable.header.status'),
-      cell: (props) => <StatusChip status={props.getValue()} />,
-    }),
-    columnHelper.accessor('id', {
-      header: t('EntryTable.header.id'),
-    }),
-    columnHelper.accessor('transactionId', {
-      header: t('EntryTable.header.transaction'),
-    }),
-  ];
+  const colDefs = useMemo(
+    () => [
+      columnHelper.accessor('transactionDate', {
+        header: t('EntryTable.header.date'),
+        cell: (props) => (
+          <FormattedDate
+            dateTime={props.getValue()}
+            className="text-dark-shades"
+          />
+        ),
+      }),
+      columnHelper.accessor('debit', {
+        header: t('EntryTable.header.debit'),
+      }),
+      columnHelper.accessor('credit', {
+        header: t('EntryTable.header.credit'),
+      }),
+      columnHelper.accessor('account.name', {
+        header: t('EntryTable.header.account'),
+      }),
+      columnHelper.accessor('memo', {
+        header: t('EntryTable.header.memo'),
+      }),
+      columnHelper.accessor('status', {
+        header: t('EntryTable.header.status'),
+        cell: (props) => <StatusChip status={props.getValue()} />,
+      }),
+      columnHelper.accessor('id', {
+        header: t('EntryTable.header.id'),
+      }),
+      columnHelper.accessor('transactionId', {
+        header: t('EntryTable.header.transaction'),
+      }),
+    ],
+    [t],
+  )
 
-  return <Table data={data} colDefs={colDefs} />;
-};
+  return <Table data={data} colDefs={colDefs} />
+}
