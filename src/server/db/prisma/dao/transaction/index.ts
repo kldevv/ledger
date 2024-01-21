e2290957793e571c184a878f65cfe0046a860135
Kuan-You Lin <linstudyhard1997@gmail.com>
2024-01-20 23:36:09 -0500

    Add transaction dao groupByCategory and groupByCategoryType

--- src/server/db/prisma/dao/transaction/index.ts
+++ src/server/db/prisma/dao/transaction/index.ts
@@ -1,5 +1,7 @@
 export * from './createOne'
 export * from './groupByAccount'
+export * from './groupByCategory'
+export * from './groupByCategoryType'
 export * from './groupByMonthAndAccount'
 export * from './groupByMonthAndCategory'
 export * from './groupByMonthAndCategoryType'
