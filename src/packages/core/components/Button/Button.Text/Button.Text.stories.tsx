933b632501ec0e83c5163fefb85ce8bacbde1f96
Kuan-You Lin <linstudyhard1997@gmail.com>
2024-05-25 14:51:43 -0400

    Add pacakges

--- src/packages/core/components/Button/Button.Text/Button.Text.stories.tsx
+++ src/packages/core/components/Button/Button.Text/Button.Text.stories.tsx
@@ -6,6 +6,7 @@ const meta: Meta<typeof ButtonText> = {
   component: ButtonText,
   args: {
     label: 'Button.Link',
+    variant: 'primary',
   },
 }
 
@@ -13,14 +14,21 @@ export default meta
 
 type Story = StoryObj<typeof ButtonText>
 
-export const Primary: Story = {
-  args: {
-    variant: 'primary',
-  },
-}
+export const Primary: Story = {}
 
 export const Secondary: Story = {
   args: {
     variant: 'secondary',
   },
+}
+
+export const Loading: Story = {
+  args: {
+    loading: true,
+  },
+  render: (args) => (
+    <div className="h-5">
+      <ButtonText {...args} />
+    </div>
+  ),
 }
---
+++ src/packages/core/components/LoadingSpinner/LoadingSpinner.stories.tsx
@@ -0,0 +1,16 @@
+import { LoadingSpinner } from './LoadingSpinner'
+
+import type { Meta, StoryObj } from '@storybook/react'
+
+const meta: Meta<typeof LoadingSpinner> = {
+  component: LoadingSpinner,
+}
+
+export default meta
+type Story = StoryObj<typeof LoadingSpinner>
+
+export const Primary: Story = {
+  args: {
+    className: 'size-8',
+  },
+}
