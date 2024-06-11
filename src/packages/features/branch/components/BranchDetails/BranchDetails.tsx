276d175ffe82b3dd0a946f44ad9bb7a6182483fd
Kuan-You Lin <linstudyhard1997@gmail.com>
2024-06-11 00:40:05 -0400

    Add action button

--- src/packages/features/branch/components/BranchDetails/BranchDetails.tsx
+++ src/packages/features/branch/components/BranchDetails/BranchDetails.tsx
@@ -2,7 +2,7 @@ import { useRouter } from 'next/router'
 import { Trans, useTranslation } from 'next-i18next'
 
 import { useBranchQuery } from '@/api/graphql'
-import { Link } from '@/packages/core/components'
+import { Button, Link } from '@/packages/core/components'
 import { PageHeader } from '@/packages/layout'
 import { route } from '@/shared/route'
 
@@ -40,7 +40,20 @@ export const BranchDetails: React.FC = () => {
             }}
           />
         }
-      />
+      >
+        <div className="flex w-40 items-center gap-x-1 text-sm">
+          <Button variant="secondary" className="flex-1">
+            {t`common:delete`}
+          </Button>
+          <Link
+            variant="primary"
+            href={{ pathname: route.branch.edit.pathname, query: { id } }}
+            className="flex-1"
+          >
+            {t`common:edit`}
+          </Link>
+        </div>
+      </PageHeader>
       <BranchDescList branch={data?.branch} loading={loading} />
     </div>
   )
